4b6b6371797c94e3e52f5f5040e28df14486c762
gprashant-src <gprashant.code@gmail.com>
2026-02-26 23:42:14 +0530

    fix: prefix, used functions for construction and range query

--- Prefix Sum/prefix.py
+++ Prefix Sum/prefix.py
@@ -1,10 +1,17 @@
 arr = [1, 4, 3, 2, 6, 10, 8]
 
-P = [0] * (len(arr) + 1)
+def prefix1d(arr):
+    P = [0] * (len(arr) + 1)
 
-for i in range(len(arr)):
-    P[i + 1] = P[i] + arr[i]
+    for i in range(len(arr)):
+        P[i + 1] = P[i] + arr[i]
 
+    return P
+
+def range1d(l, r, P):
+    return P[r + 1] - P[l]
 l, r = 2, 5
-print(P)
-print("Range sum=",  P[r + 1] - P[l])
+
+P = prefix1d(arr)
+# print(P)
+print("Range sum=",range1d(l, r, P))
