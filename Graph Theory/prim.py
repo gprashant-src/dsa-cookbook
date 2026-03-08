119db18de3a0519315e2e57fc4de820957ff7655
gprashant-src <gprashant.code@gmail.com>
2026-03-08 13:04:06 +0530

    fix: prim.py -> Implementation modify

--- Graph Theory/prim.py
+++ Graph Theory/prim.py
@@ -6,26 +6,27 @@ n = 5
 edges = [(0,1,1),(0,2,7),(1,2,5),(1,3,4),(3,4,2),(2,4,6),(1,4,3)]
 
 MST = set()
-for u, v, e in edges:
-    heapq.heappush(graph[v], (e, u))
-    heapq.heappush(graph[u], (e, v))
 
-start = 0
-MST.add(start)
+for u, v, e in edges:
+    graph[u].append((e, v))
+    graph[v].append((e, u))
+
+MST = set()
 cost = 0
+heap = [(0, 0)]
 
 while len(MST) < n:
-    temp = list(MST)
-    for u in temp:
-        e,v = heapq.heappop(graph[u])
-        if v not in MST:
-            MST.add(v)
-            cost += e
-            continue
-        heapq.heappush(graph[u], (e, v))
+    w, u = heapq.heappop(heap)
+    if u in MST:
+        continue
+
+    MST.add(u)
+    cost += w
+
+    for edge in graph[u]:
+        heapq.heappush(heap, edge)
 
 print(cost)
-
 
 
     
